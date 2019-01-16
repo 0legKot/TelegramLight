f74719255a88a89c6309ee990efbe9db207583e4
Oleg <kronosgod12@gmail.com>
2019-01-16 13:28:07 +0200

    Initial

--- TelegramLight/MainWindow.xaml.cs
+++ TelegramLight/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TeleSharp.TL;
+using TLSharp.Core;
 
 namespace TelegramLight
 {
@@ -20,9 +23,44 @@ namespace TelegramLight
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int apiId = 0;
+        const string apiHash = "";
+        string hash = "";
+        string code = "";
+        static readonly TelegramClient client= new TelegramClient(apiId, apiHash);
         public MainWindow()
         {
             InitializeComponent();
+            rtbMain.Document.Blocks.Clear();
+        }
+
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            TLUser user = await client.MakeAuthAsync(TbPhone.Text, hash, TbCode.Text);
+            
+            if (client.IsUserAuthorized())
+            {
+                //get available contacts
+                var result = await client.GetContactsAsync();
+                rtbMain.AppendText(result.Contacts.Count.ToString()+"\n");
+                foreach (var item in result.Users.OfType<TLUser>())
+                {
+                    rtbMain.AppendText(item.Username+"\n");
+                }
+                var kostya = result.Users
+                    .OfType<TLUser>()
+                    .FirstOrDefault(x => x.Username == "KostyaNes");
+                await client.SendMessageAsync(new TLInputPeerUser() { UserId = kostya.Id }, "Hello from my stupid telegram app");
+            }
+        }
+
+        private async void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            await client.ConnectAsync();
+
+            hash = await client.SendCodeRequestAsync(TbPhone.Text);
         }
     }
 }
